abf089182fba25ab5d4456bed2896792fc92ffd4
Mac <macclinson@yahoo.com>
2014-04-07 18:24:59 +0100

    Initial Commit

---
+++ js/scale.js
@@ -0,0 +1,37 @@
+jQuery(function ($) {
+    resizeImages();
+    
+    $('#home-slider img').load(resizeImages);
+    $(window).resize(resizeImages);
+});
+
+function resizeImages () {
+console.log('Resizing');
+    var imgs = $('#home-slider img');
+    var win = $('#home-slider');
+    var width = win.width();
+    var height = win.height();
+    
+    // For every image...
+    imgs.each(function () {
+        var img = $(this);
+        
+        // What shall we resize?
+        var widthRatio = img.width() / width;
+        var heightRatio = img.height() / height;
+        var lowerRatio = Math.min(widthRatio, heightRatio);
+        
+        img.removeAttr('width').removeAttr('height');
+        
+        if(widthRatio === lowerRatio) {
+            img.attr('width', width);
+        } else {
+            img.attr('height', height);
+        }
+        
+        // Center
+        img.css('top', /*(height - img.height()) / 2*/ 0); // The top of the image should always be visible
+        img.css('left', (width - img.width()) / 2);
+    });
+}
+
